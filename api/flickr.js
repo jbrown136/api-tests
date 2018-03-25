963b16e6c789c94799bf2ee5ed57e727c5ea3496
jbrown136 <35295204+jbrown136@users.noreply.github.com>
2018-03-25 20:04:52 +0100

    Update flickr.js

--- api/flickr.js
+++ api/flickr.js
@@ -8,22 +8,11 @@ flickrOptions = {
 //     api_key: config.key
 // });
 Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-    const photos = flickr.photos.search({
+    flickr.photos.search({
         user_id: flickr.options.user_id,
         page: 1,
         per_page: 20
       }, function(err, result) {
         console.log(result)
-      });
-      //console.log(photos)    
+      });    
 });
-
-// const photos = flickr.photos.search({
-//     user_id: flickr.options.user_id,
-//     page: 1,
-//     per_page: 20
-//   }, function(err, result) {
-//     // result is Flickr's response
-//   });
-
-  //console.log(photos)
